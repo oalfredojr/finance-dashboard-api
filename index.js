b41be66f12fff58d8c7f6bb00638ce752d6c7434
Alfredo Corrêa Lima Junior <contatoalfredo@live.com>
2025-11-18 21:06:26 -0300

    chore: express project

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('teste de lint')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
