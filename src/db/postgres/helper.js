dc69e5670ce763782f35da4bc7c05eb604fdacee
Alfredo Corrêa Lima Junior <contatoalfredo@live.com>
2025-11-24 20:58:36 -0300

    feat: add init migration to create users and transactions table

--- src/db/postgres/helper.js
+++ src/db/postgres/helper.js
@@ -5,8 +5,6 @@ dotenv.config()
 
 const { Pool } = pg
 
-console.log(process.env.POSTGRES_PASSWORD)
-
 export const pool = new Pool({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
