f50b5f3d85c81c1cf21c0d8bfc12f0709669508f
Alfredo Corrêa Lima Junior <contatoalfredo@live.com>
2025-11-28 20:03:27 -0300

    feat: add CreateUserCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcryptjs'
+
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUseCase {
+    async execute(createUserPARAMS) {
+        // TODO: verificar se o e-mail já esta em uso
+
+        // gerar ID do usuário
+        const userId = uuidv4()
+        // criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserPARAMS.password, 10)
+        // inserir o usuário no banco de dados
+        const user = {
+            ...createUserPARAMS,
+            id: userId,
+            password: hashedPassword,
+        }
+
+        // chamar o repositório
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await postgresCreateUserRepository.execute(user)
+
+        return createdUser
+    }
+}
