faba5901aa17c1b0abb916399d425150e0f1e10f
@oalfredojrdev <jubabadboy@hotmail.com>
2026-03-10 18:42:10 -0300

    Refactor: unify DB helper and extract user router

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 import 'dotenv/config.js'
 import express from 'express'
-import { CreateUserController } from './src/controllers/create-user.js'
-import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
-import { UpdateUserController } from './src/controllers/update-user.js'
+import usersRouter from './src/routes/users.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/api/users', async (request, response) => {
-    const createUserController = new CreateUserController()
-
-    const { statusCode, body } = await createUserController.execute(request)
-
-    response.status(statusCode).json(body)
-})
-
-app.get('/api/users/:userId', async (request, response) => {
-    const getUserByIdController = new GetUserByIdController()
-
-    const { statusCode, body } = await getUserByIdController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.patch('/api/users/:userId', async (request, response) => {
-    const updateUserController = new UpdateUserController()
-
-    const { statusCode, body } = await updateUserController.execute(request)
-
-    response.status(statusCode).json(body)
-})
+// mount user router
+app.use('/api/users', usersRouter)
 
 app.listen(process.env.PORT, () =>
     console.log(`Listening on port ${process.env.PORT}`),
